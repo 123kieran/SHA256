129864acff8c596cb22713a31724c66fb15cde56
123kieran <123kieran@gmail.com>
2019-03-04 21:23:10 +0000

    added function and some variables

--- sha256.c
+++ sha256.c
@@ -1,1 +1,38 @@
+// Author: Kieran O'Halloran 
+// Program: SHA-256 Algorithm implentation in C
 
+
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
+
+ void sha256();
+ // Main method
+ int main(int argc, char *argv[]) 
+ {
+      // Print header
+       printf("\n======== SHA256 - HASHING ALGORITHM ========\n");
+
+                // Function calls
+                 sha256();
+                         
+                 // Main return method
+                 return 0;
+               }
+
+                 void sha256()
+               {
+                 // Message schedule
+                 __uint32_t W[64];
+
+                // Working variables
+                 __uint32_t a, b, c, d, e, f, g, h;
+
+                // Hash value
+                 __uint32_t H[8];
+
+                // Temp variables
+                 __uint32_t T1;
+                 __uint32_t T2;
+                }
+               }
