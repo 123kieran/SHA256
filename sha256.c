4ba840681813593a3db2ae8744d25c9ed6c5678d
123kieran <123kieran@gmail.com>
2019-03-14 14:00:30 +0000

    added hash values

--- sha256.c
+++ sha256.c
@@ -1,38 +1,39 @@
-// Author: Kieran O'Halloran 
-// Program: SHA-256 Algorithm implentation in C
-
+// Author: Kieran O'Halloran
+// The Secure Hash Algorithm, 256 bit Version
 
  #include <stdio.h>
- #include <stdlib.h>
- #include <string.h>
+ #include <stdint.h>
 
  void sha256();
- // Main method
- int main(int argc, char *argv[]) 
- {
-      // Print header
-       printf("\n======== SHA256 - HASHING ALGORITHM ========\n");
 
-                // Function calls
-                 sha256();
-                         
-                 // Main return method
-                 return 0;
-               }
+ int main(int argc, char *argv[]){
 
-                 void sha256()
-               {
-                 // Message schedule
-                 __uint32_t W[64];
+   sha256();
 
-                // Working variables
-                 __uint32_t a, b, c, d, e, f, g, h;
+     return 0;
+     }
 
-                // Hash value
-                 __uint32_t H[8];
+  void sha256(){
+       
+      //Message Schedule
+        uint32_t W[64];
 
-                // Temp variables
-                 __uint32_t T1;
-                 __uint32_t T2;
-                }
-               }
+      //Working variables
+        uint32_t a, b, c, d, e, f, g;
+                 
+      //Temporary Variables
+        uint32_t T1, T2;
+
+      // The Hash value.
+        uint32_t H[8] = {
+         0x6a09e667,
+         0xbb67ae85,
+         0x3c6ef372,
+         0xa54ff53a,   
+         0x510e527f,
+         0x9b05688c,
+         0x1f83d9ab,
+         0x5be0cd19
+ 	};
+
+  } 
