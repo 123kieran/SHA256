63b6bb413d03e317620f84ae21fc81ce6b870fc2
123kieran <123kieran@gmail.com>
2019-03-23 15:52:07 +0000

    opening file and reading byte

--- padfile.c
+++ padfile.c
@@ -4,5 +4,13 @@
 int main(int argc, char *argv[]) {
        	FILE* f;
 
+	 char c;
+
+         f = fopen(argv[1], "r");
+
+         printf("%c\n", fread(&c, 1, 1, f));
+
+	 fclose(f);
+
 	return 0;
 }
