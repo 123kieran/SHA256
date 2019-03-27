975b56742b73a2b11861ff733f24def37a802c65
123kieran <123kieran@gmail.com>
2019-03-27 16:51:26 +0000

    moving padfile to sha256

--- sha256.c
+++ sha256.c
@@ -3,11 +3,17 @@
 
  #include <stdio.h>
  #include <stdint.h>
-
- void sha256();
+//Represents a message block
+ union msgblock {
+    uint8_t e[64];
+    uint32_t t[16];
+    uint64_t s[8];
+};
+//A flag for where we are in reading the file
+ enum status {READ, PAD0, PAD1, FINISH};
 
  //see section 4.1.2
- int32_t sig0(uint32_t x);
+ uint32_t sig0(uint32_t x);
  uint32_t sig1(uint32_t x);
 
  //see section 3.2.1
@@ -22,41 +28,58 @@
  uint32_t Ch(uint32_t x, uint32_t y, uint32_t z);
  uint32_t Maj(uint32_t x, uint32_t y, uint32_t z);
 
-//The K Constants. Defined in Section 4.2.2
-    uint32_t K[]={
-       0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 
-       0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5, 
-       0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 
-       0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174, 
-       0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 
-       0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da, 
-       0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 
-       0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967, 
-       0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 
-       0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85, 
-       0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 
-       0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070, 
-       0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 
-       0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3, 
-       0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 
-       0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2 
-          };
+ //Calculates the SHA256 of a file
+ void sha256(File *f);
 
+//Retrives the next message block
+ int nextmsgblock(FILE *f, union msgblock, enum status *S, int *nobites);
 
-
-
+ //Starting everything
  int main(int argc, char *argv[]){
 
-   sha256(); 
+	 //ADD ERROR CHECKING
+      FILE* f;
+      f = fopen(argv[1], "r");
+     
+      sha256(f); 
 
-     return ;
+      return 0 ;
      }
 
 //sha256 function (return 256 message digest) array of 32 bit integers
   void sha256(){
+     
+	  //The current message block
+       union msgblock M;
 
-      //Message Schedule
-      unit32_t W[64]
+       //The number of bits read from the file
+       uint64_t nobits=0;
+
+       //The ststus of the message blocks, in terms of padding	        
+       enum status S = READ;
+
+      //The K Constants. Defined in Section 4.2.2
+          uint32_t K[]={
+            0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 
+            0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5, 
+            0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 
+            0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174, 
+            0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 
+	    0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da, 
+	    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 
+	    0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967, 
+	    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 
+	    0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85, 
+	    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 
+	    0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070, 
+	    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 
+	    0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3, 
+	    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 
+	    0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2 
+	  };
+	  
+	  //Message Schedule
+     uint32_t W[64];
       //Working variables;
        uint32_t a,b,c,d,e,f,g,h;
       //Temporary Variables
@@ -74,23 +97,22 @@
          0x5be0cd19
  	};
 
-
-  //the  current message block
-    uint32_t M[16] = {0,0,0,0,0,0,0,0};
       
   // for looping 
      int i,t;
 
-     for (i=0; i<1;i++){
+     //loop through message blocks as per page 22
+     while (nextmsgblock(f, M, S, nobits)){
+
   
        for(t =0; t <  16; t++)
-           W[t] = M[t];
+           W[t] = M.t[t];
   
-         for(t = 16; t <64; t++)
-             sig1(W[t-2]) + W[t-7] + sig0(W[t-15]) + W[t-16];
+       for(t = 16; t <64; t++)
+          W[t] = sig1(W[t-2]) + W[t-7] + sig0(W[t-15]) + W[t-16];
 	     //initialise a ,b, c. d, e, f, g, h.
-	      a = H[0]; b = H[1]; c = H[2]; d = H[3]; e = H[4];
-	      f = H[5], g = H[6], h = H[7];
+	      a = H[0]; b = H[1]; c = H[2]; d = H[3];
+	      e = H[4]; f = H[5], g = H[6], h = H[7];
 	     
 	     //Step 3
 	      for (t = 0; t < 64; t++){
@@ -123,41 +145,79 @@
      printf("%x, %x, %x, %x, %x, %x, %x, %x\n", H[0], H[1], H[2], H[3], H[4], H[5], H[6], H[7]);
   }
   
-       uint32_t rotr(uint32_t n, uint32_t x){
-           return (x >> n) | (x << (32 -n));
-       }
-      uint32_t shr(uint32_t n, uint32_t x){
-                return(x >> n);
-	     
-	       }
-	     
-     uint32_t sig0(uint32_t x){
-     return(rotr(7, x) ^ rotr(18, x) ^ shr(3, x));
-                                                                                                                                        
-               }
-    uint32_t sig1(uint32_t x){
-      return(rotr(17, x) ^ rotr(19, x) ^ shr(10, x));
+ uint32_t rotr(uint32_t n, uint32_t x){
+   return (x >> n) | (x << (32 -n));
+  }
 
+ uint32_t shr(uint32_t n, uint32_t x){
+   return(x >> n);
+  }
 
+ uint32_t sig0(uint32_t x){
+   return(rotr(7, x) ^ rotr(18, x) ^ shr(3, x));                                   }
 
+ uint32_t sig1(uint32_t x){
+   return(rotr(17, x) ^ rotr(19, x) ^ shr(10, x));
+  }
      //See Section 4.1.2
-    uint32_t SIG0(uint32_t x){
-     return (rotr(2, x) ^ rotr(13, x) ^ rotr(22, x));
+ uint32_t SIG0(uint32_t x){
+   return (rotr(2, x) ^ rotr(13, x) ^ rotr(22, x));
+  }
 
- }
-    uint32_t SIG1(uint32_t x){
-     return (rotr(6, x) ^ rotr(11, x) ^ rotr(25, x));
+ uint32_t SIG1(uint32_t x){
+   return (rotr(6, x) ^ rotr(11, x) ^ rotr(25, x));
+  }
 
-   }
+ uint32_t Ch(uint32_t x, uint32_t y, uint32_t z){
+   return((x & y) ^ ((!x) & z));
+  }
 
-   uint32_t Ch(uint32_t x, uint32_t y, uint32_t z){
-     return((x & y) ^ ((!x) & z));
-
-       }
-   uint32_t Maj(uint32_t x, uint32_t y, uint32_t z){
-     return((x & y) ^ (x & z) ^ (y & z));
-         
-
-	    
+ uint32_t Maj(uint32_t x, uint32_t y, uint32_t z){
+   return((x & y) ^ (x & z) ^ (y & z));	    
   }
    
+int nextmsgblock(FILE *f, union msgblock *M, enum status *S, int *nobits){
+ 	//The number of bytes we get from fread
+     uint64_t nobytes;
+
+   
+	//for looping
+     int i;
+
+     while (S == READ){
+     nobytes = fread(M.e,1,64,f);
+
+     printf("Read %2llu bytes\n", nobytes);	
+     	  nobits = nobits + (nobytes * 8);									  	  if (nobytes < 56) {
+											  		  printf("Ive found a block with less than 55 bytes!\n");
+												  		  M.e[nobytes] = 0x80;
+												  		  while (nobytes < 56) {	
+					 						  		    nobytes = nobytes + 1;
+												    	       	    M.e[nobytes] = 0x00;
+												    		  }							     
+											  		   // Setting last 8 bytes as 64 bit integer
+												   		    M.s[7] = nobits;
+										    		    		    S = FINISH;
+     }
+							    		    		                  	  else if(nobytes <64){
+	  S=PAD0;
+	  M.e[nobytes]=0x80;
+
+	  		    		                  	  						while(nobytes<64){	 		    		                 
+														nobytes = nobytes +1;										 	  M.e[nobytes] = 0x00;
+														}
+	  		    		                  	  																			}
+	  		    		                  	  																			//Figuring out of finnished reading and happens to be exactly 512 bytes		    		    		    		                  	  							
+	else if(feof(f)){											S = PAD1;																						        		}
+												        	if(S == PAD0|| S == PAD1)
+	for(int i = 0;i<56;i++){ 		    		                  	  			   M.e[i] = 0x00;					 	     
+	 M.s[7] = nobits;
+	}
+	}
+														if(S == PAD1){							  		    		             M.e[0] = 0x80;										  	}														      
+     fclose(f);	    		                  	  																														        		    for(int i = 0; i < 64; i++){
+	 printf("%x", M.e[i]);
+	 printf("\n"); 
+	 }				   
+														 return 0;
+    }
